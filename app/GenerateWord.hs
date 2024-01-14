3b4d24a850fbc5783761ef8287dec255ccfbf05f
Doncho <nardo0905@outlook.com>
2024-01-14 16:36:02 +0200

    Update word file and fix file paths

--- app/GenerateWord.hs
+++ app/GenerateWord.hs
@@ -5,7 +5,7 @@ import System.Random (Random (randomR), getStdRandom)
 -- This function takes a file with words and filters only the words with length n in it and puts them in a list
 filterWordList :: Int -> IO [String]
 filterWordList n = do
-  wordFile <- readFile "data/words_alpha.txt"
+  wordFile <- readFile "data/30k.txt"
   let wordsList = words wordFile
   let filteredList = filter (\x -> length x == n) wordsList
   return filteredList
